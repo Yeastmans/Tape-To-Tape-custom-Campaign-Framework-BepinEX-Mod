4d37ff22e5a2d206e28743d49f8a83af5977d854
Yeastmans <kiranspring99@gmail.com>
2026-04-30 12:12:39 -0700

    v2.1.12 - PyInstaller onedir mode (fixes Failed to load Python DLL)

--- src/CustomCampaignFramework/Plugin.cs
+++ src/CustomCampaignFramework/Plugin.cs
@@ -23,7 +23,7 @@ using Rogue.BenchSnapshots;
 
 namespace EndlessMode;
 
-[BepInPlugin("com.mods.customcampaign", "Custom Campaign Framework", "2.1.11")]
+[BepInPlugin("com.mods.customcampaign", "Custom Campaign Framework", "2.1.12")]
 public class Plugin : BasePlugin
 {
     internal static new ManualLogSource Log;
